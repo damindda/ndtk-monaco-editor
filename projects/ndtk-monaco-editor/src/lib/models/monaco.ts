c3cd3101edb4ddda9942616fbb4338b8aaee2590
damindda <damindda@gmail.com>
2022-07-28 09:07:01 +0100

    fix: declare monaco

--- projects/ndtk-monaco-editor/src/lib/models/monaco.ts
+++ projects/ndtk-monaco-editor/src/lib/models/monaco.ts
@@ -1,3 +1,4 @@
+declare let monaco: any;
 export declare type MonacoEditorOptions = monaco.editor.IStandaloneEditorConstructionOptions;
 export declare type MonacoEditor = monaco.editor.IStandaloneCodeEditor;
 export declare type LineNumbers = 'on' | 'off';
